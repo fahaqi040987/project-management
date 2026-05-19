fdeb1689807d1e06185025385234fa89cac7d123
Fajar Ardhi Haqi <fajarardhi040987@gmail.com>
2026-05-19 13:17:26 +0700

    feat: initialize Filament AdminPanelProvider with custom login, shield integration, and theme configuration

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -42,7 +42,6 @@ class AdminPanelProvider extends PanelProvider
                 Dashboard::class,
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
-            ->widgets([])
             ->middleware([
                 EncryptCookies::class,
                 AddQueuedCookiesToResponse::class,

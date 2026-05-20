5c88e6a818b71d667e798f7f7f8ca21af5f38af0
Fajar Ardhi Haqi <fajarardhi040987@gmail.com>
2026-05-20 12:24:18 +0700

    adding new feature ticket category

---
+++ app/Models/TicketCategory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class TicketCategory extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'project_id',
+        'name',
+        'description',
+    ];
+
+    public function project(): BelongsTo
+    {
+        return $this->belongsTo(Project::class);
+    }
+
+    public function tickets(): HasMany
+    {
+        return $this->hasMany(Ticket::class);
+    }
+}
